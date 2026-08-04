f95a88f653d2001aa2b380928e50405cceda9088
prahzera <cejotarodrigues@gmail.com>
2026-08-04 14:25:02 -0300

    KoC Power Bot Plus v3.69 - Bump version to 3.69

--- script.js
+++ script.js
@@ -41,13 +41,13 @@
 // @original-license            http://creativecommons.org/licenses/by/4.0/
 // @original-changes            Updated to include latest items from KoC
 // @original-author             barbarossa69
-// @version			3.68
+// @version			3.69
 // @releasenotes	        Eagle Warrier changes updated
 // @downloadURL https://github.com/prahzera/KoC-PowerBotPlus/releases/latest/download/script.js
 // @updateURL https://github.com/prahzera/KoC-PowerBotPlus/releases/latest/download/script.meta.js
 // ==/UserScript==
 
-var Version = '3.68';
+var Version = '3.69';
 var SourceName = "Power Bot Plus";
 function GlobalOptionsUpdate() {
 }
